0773a9e20bc2a3454dd4dcba17c46d8940d24e82
Andriks Imanol Ruiz Martínez <105221206+Ruiz-Martinez-Andriks-Imanol@users.noreply.github.com>
2022-05-15 09:47:56 -0500

    Update tarea1.2.cpp

--- tarea1.2.cpp
+++ tarea1.2.cpp
@@ -1,32 +1,31 @@
-/* Autor: Andriks Imanol Ruiz Mart韓ez, Realizado: 03/02/2022
+/* Autor: Andriks Imanol Ruiz Mart铆nez, Realizado: 03/02/2022
 Escuela: Universidad del Valle de Mexico Campus Villahermosa 
-Materia: Programaci髇 Estructrada 
+Materia: Programaci贸n Estructrada 
 Ciclo: 01/2022
 
-Este es un programa en Lenguaje C de la materia de Programaci髇 Estructurada<br>
+Este es un programa en Lenguaje C de la materia de Programaci贸n Estructurada<br>
 Muestra el uso de:
   
     -Variables flotantes
     -printf para mostrar mensajes y variables
     -scanf
-    -El uso de include para las librer韆s
-    -uso de la librer韆 math.h
-    -Comentarios para la documentaci髇 interna del programa
+    -El uso de include para las librer铆as
+    -uso de la librer铆a math.h
+    -Comentarios para la documentaci贸n interna del programa
 	*/	
 #include <stdio.h>
 #include<math.h>
 int main() {
-	//Declaraci髇 de variable
+	//Declaraci贸n de variable
 float r, a, v;
 	//Entrada de datos
-	printf("羠ea de la esfera \n");
+	printf("脕rea de la esfera \n");
 	printf("Introduce el radio de la esfera: ");
 	scanf("%f", &r);
 	//proceso
 	a=(4)*M_PI*pow(r,2);
 	//Salida
 	printf("El area de la esfera es %f", a);
-	
 	printf("volumen de la esfera \n");
 	//Entrada de los datos
 	printf("Introduce el volumen de la esfera: ");
